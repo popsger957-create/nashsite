fedafc76df71b0ff7e77e44e7b78fac0275f03ab
purple-zephyr <popsger957@gmail.com>
2026-04-24 19:56:18 +0300

    онлайн

--- script.js
+++ script.js
@@ -15,3 +15,30 @@ clickBtn.addEventListener('click', () => {
     }
         
 });
+
+
+
+
+    async function updateOnlineStatus() {
+        const url = 'https://api.allorigins.win/get?url=' + encodeURIComponent('https://countrymeters.info/ru/World');
+        
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(data.contents, 'text/html');
+            
+            // Берем число из источника
+            const population = doc.getElementById('cp1').innerText;
+            
+            // Вставляем в твой <h3>
+            document.getElementById('online-count').innerText = population;
+        } catch (e) {
+            console.log("Ошибка обновления");
+        }
+    }
+
+    // Запускаем
+    updateOnlineStatus();
+    // Обновляем раз в 10 секунд для реализма
+    setInterval(updateOnlineStatus, 10000);
